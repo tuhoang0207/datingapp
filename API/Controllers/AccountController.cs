e493fba671e2e9803d28cb10db558709e9eb55ea
tuhoang0207 <tuhoang0207@gmail.com>
2023-06-21 11:45:12 +0700

    end of section 7

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -22,17 +22,20 @@ namespace API.Controllers
             _tokenService = tokenService;
             _context = context;
         }
- 
+
         [HttpPost("register")]
-        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto) {
-            if(await UserExists(registerDto.UserName)) {
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.Username))
+            {
                 return BadRequest("Username is taken");
             }
 
             using var hmac = new HMACSHA512();
 
-            var user  = new AppUser {
-                UserName = registerDto.UserName.ToLower(),
+            var user = new AppUser
+            {
+                UserName = registerDto.Username.ToLower(),
                 PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
@@ -40,17 +43,20 @@ namespace API.Controllers
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
-            return new UserDto{
+            return new UserDto
+            {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user)
-            };        
+            };
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto) {
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
             var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
 
-            if(user == null) {
+            if (user == null)
+            {
                 return Unauthorized("Invalid username");
             }
 
@@ -58,18 +64,22 @@ namespace API.Controllers
 
             var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
 
-            for(int i = 0; i < computedHash.Length; i++) {
-                if(computedHash[i] != user.PasswordHash[i]) {
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i])
+                {
                     return Unauthorized("Invalid password");
                 }
             }
 
-            return new UserDto{
+            return new UserDto
+            {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user)
             };
         }
-        private async Task<bool> UserExists(string userName) {
+        private async Task<bool> UserExists(string userName)
+        {
             return await _context.Users.AnyAsync(u => u.UserName == userName.ToLower());
         }
     }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -26,11 +26,7 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe(
       (response) => {
         this.router.navigateByUrl('/members');
-      },
-      (error) => {
-        console.log(error);
-        this.toastr.error(error.error);
-      });
+      })
   }
 
   logout() {
