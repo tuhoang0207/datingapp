d12a049e03611777b59c4fac19940697bc7bdaf2
tuhoang0207 <tuhoang0207@gmail.com>
2023-06-23 15:50:59 +0700

    end of section 8

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using AutoMapper;
 
 namespace API.Helpers
@@ -11,7 +12,10 @@ namespace API.Helpers
     public class AutoMapperProfiles : Profile
     {
         public AutoMapperProfiles() {
-            CreateMap<AppUser, MemberDto>();
+            CreateMap<AppUser, MemberDto>()
+            .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => 
+                src.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotoDto>();
         }        
     }
